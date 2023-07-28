271f7f620b25c12c63bdf29dc4172629425e9994
S261na <sabinastalbekova258@gmail.com>
2023-07-28 17:48:08 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+//выбираем сам лифт
+let elevator = document.querySelector('.elevator');
+
+//выбираем этажи
+let firstFloor = document.querySelector('.first-floor');
+let secondFloor = document.querySelector('.second-floor');
+let thirdFloor = document.querySelector('.third-floor');
+let fouthFloor = document.querySelector('.fourth-floor');
+let fifthFloor = document.querySelector('.fifth-floor');
+
+//выбираем кнопки вверх и вниз
+let btnUp = document.querySelector('.buttonUp');
+let btnDown = document.querySelector('.buttonDown');
+
+//запрашиваем у пользователя нужный лифт
+let btn = document.querySelector('.btn');
+
+let selectedFloor = 0;
+btn.addEventListener('click', () => {
+  selectedFloor = +prompt('Введите нужный этаж', []);
+  switch (selectedFloor) {
+    case 1:
+      elevator.style.marginBottom = '0';
+      break;
+    case 2:
+      elevator.style.marginBottom = '100px';
+      break;
+    case 3:
+      elevator.style.marginBottom = '200px';
+      break;
+    case 4:
+      elevator.style.marginBottom = '300px';
+      break;
+    case 5:
+      elevator.style.marginBottom = '400px';
+      break;
+  }
+});
+
+//реализуем перемещение лифта при нажатии нужной кнопки
+firstFloor.addEventListener('click', () => {
+  if (elevator.style.marginBottom !== 0) {
+    elevator.style.marginBottom = '0';
+  }
+});
+secondFloor.addEventListener('click', () => {
+  elevator.style.marginBottom = '100px';
+});
+thirdFloor.addEventListener('click', () => {
+  elevator.style.marginBottom = '200px';
+});
+fouthFloor.addEventListener('click', () => {
+  elevator.style.marginBottom = '300px';
+});
+fifthFloor.addEventListener('click', () => {
+  elevator.style.marginBottom = '400px';
+});
+
+//перемещаем лифт вверх
+btnUp.addEventListener('click', () => {
+  switch (elevator.style.marginBottom) {
+    case '':
+    case '0px':
+      elevator.style.marginBottom = '100px';
+      break;
+    case '100px':
+      elevator.style.marginBottom = '200px';
+      break;
+    case '200px':
+      elevator.style.marginBottom = '300px';
+      break;
+    case '300px':
+      elevator.style.marginBottom = '400px';
+      break;
+  }
+});
+
+//перемещаем лифт вниз
+btnDown.addEventListener('click', () => {
+  switch (elevator.style.marginBottom) {
+    case '400px':
+      elevator.style.marginBottom = '300px';
+      break;
+    case '300px':
+      elevator.style.marginBottom = '200px';
+      break;
+    case '200px':
+      elevator.style.marginBottom = '100px';
+      break;
+    case '100px':
+      elevator.style.marginBottom = '0';
+      break;
+  }
+});
+
+// let currentPosition = elevator.getBoundingClientRect().top;
